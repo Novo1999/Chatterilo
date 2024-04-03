3168aef3e9d2779545baebbc65ba2bbe3c574d7b
Novo1999 <neptune.navo@gmail.com>
2024-04-03 13:39:34 +0600

    fix user list width frid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <main className='bg-[#23262e] min-h-screen px-6 pt-6'>
+    <main className='bg-[#23262e] min-h-screen pl-3 pr-1 pt-3'>
       <Header />
       <Stories />
       <ChatUsers />
